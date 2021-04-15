705b095bad6544ddd4d50e22c4038569921b1268
Guilherme Jr <falecom@guilhermejr.net>
2021-04-15 09:07:07 -0300

    feat: adicionado autenticação na documentação da API

--- src/main/java/net/guilhermejr/apilivros/config/OpenApiConfig.java
+++ src/main/java/net/guilhermejr/apilivros/config/OpenApiConfig.java
@@ -3,16 +3,26 @@ package net.guilhermejr.apilivros.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class OpenApiConfig {
 	
 	@Bean
 	public OpenAPI openApi() {
+		final String securitySchemeName = "Bearer Token";
 		return new OpenAPI()
+				.addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+		        .components(new Components().addSecuritySchemes(securitySchemeName,
+		            new SecurityScheme().name(securitySchemeName)
+		                .type(SecurityScheme.Type.HTTP)
+		                .scheme("bearer")
+		                .bearerFormat("JWT")))
 				.info(new Info()
 						.title("Api de livros")
 						.description("Descrição api de livros")
