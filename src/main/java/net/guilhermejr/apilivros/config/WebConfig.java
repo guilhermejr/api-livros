b64d4bca688f3177d7f7df685eda4ab7ee09550f
Guilherme Jr <falecom@guilhermejr.net>
2021-04-25 17:41:40 -0300

    feat: adicionado cors no Spring Security

--- src/main/java/net/guilhermejr/apilivros/config/WebConfig.java
+++ src/main/java/net/guilhermejr/apilivros/config/WebConfig.java
@@ -11,8 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins("*")
-            //.allowedMethods("*");
-            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+            .allowedHeaders("*");
     }
 	
 }
